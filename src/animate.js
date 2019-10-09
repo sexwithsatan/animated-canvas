ee0a54d90f56ab8f12455ace78592d1fd46cc6e3
sex with satan <sexwithsatan@users.sf.net>
2019-10-09 15:36:17 -0700

    adjust timestamps to compensate for lag

--- src/animate.js
+++ src/animate.js
@@ -1,13 +1,24 @@
 /* global requestAnimationFrame */
-import animateFrame from './animate-frame.js'
 import nextFrame from './next-frame.js'
 
 export default
 function animate(iterable, {fps}) {
+  const interval = 1000/fps
   const handle = requestAnimationFrame(t => {
-    animateFrame(iterable, 1000/fps, t)
+    animateFrame(t)
   })
   
   nextFrame(iterable[Symbol.iterator]().next(undefined), handle)
-  return iterable
+
+  function animateFrame(now, start = now) {
+    const delta = now - start
+    const lag = delta % interval
+    const handle = requestAnimationFrame(t => {
+      animateFrame(t, now - lag)
+    })
+
+    if (delta > interval) {
+      nextFrame(iterable[Symbol.iterator]().next(delta), handle)
+    }
+  }
 }
